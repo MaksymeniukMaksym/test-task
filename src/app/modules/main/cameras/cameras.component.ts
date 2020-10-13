ef1a2944fe86832289126f099e8c88e703bafd73
Maks <maksymmaksymeniuk@gmail.com>
2020-10-13 19:06:02 +0300

    update camers

--- src/app/modules/main/cameras/cameras.component.ts
+++ src/app/modules/main/cameras/cameras.component.ts
@@ -1,19 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MathService } from 'src/app/core/services/math.service';
+import { CamerasAnimation } from './cameras.animation';
 import { CameraService } from './cameras.service';
 
 @Component({
   selector: 'app-cameras',
   templateUrl: './cameras.component.html',
   styleUrls: ['./cameras.component.scss'],
+  animations: [CamerasAnimation],
 })
-export class CamerasComponent implements OnInit {
+export class CamerasComponent implements OnInit, OnDestroy {
+  public isCameraData = false;
   public camers = [];
-  constructor(private cameraService: CameraService) {}
+  public index = 0;
+  public updateInterval;
+  private autoUpdateCamersInterval;
+  constructor(
+    private cameraService: CameraService,
+    public mathService: MathService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.cameraService.updateCamers();
     this.cameraService.getCamers().subscribe((res) => {
-      console.log(res.camers);
-      this.camers = res.camers;
+      console.log(res);
+      this.camers = res;
+      console.log(this.camers);
+      this.isCameraData = true;
     });
+    this.autoUpdateCadr();
+    this.autoUpdateCamers();
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.autoUpdateCamersInterval);
+    this.stopCadrUpdate();
+  }
+
+  private autoUpdateCamers(): void {
+    this.autoUpdateCamersInterval = setInterval(() => {
+      this.isCameraData = false;
+      this.stopCadrUpdate();
+      this.cameraService.updateCamers();
+      this.autoUpdateCadr();
+      this.index = 0;
+    }, 5000);
+  }
+
+  private stopCadrUpdate(): void {
+    clearInterval(this.updateInterval);
+  }
+  private autoUpdateCadr(): void {
+    this.updateInterval = setInterval(() => {
+      this.camers.forEach((camera) => {
+        // this.updateIntervals.push(
+        camera.active = this.mathService.getRandomInt(this.camers.length);
+        // );
+      });
+    }, 1000);
   }
 }
--- src/app/modules/main/cameras/cameras.service.ts
+++ src/app/modules/main/cameras/cameras.service.ts
@@ -2,15 +2,30 @@ import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CameraService {
+  private _camers = new BehaviorSubject<Array<any>>([]);
+  // public camers$ = this._camers.asObservable();
+
   constructor(private http: HttpClient, private router: Router) {}
 
+  public updateCamers(): void {
+    this.http
+      .get<any>(`${environment.api}/cameras`)
+      .pipe(take(1))
+      .subscribe((res) => {
+        this._camers.next(res);
+      });
+  }
   public getCamers(): Observable<any> {
-    return this.http.get<any>(`${environment.api}/cameras`);
+    return this._camers.asObservable();
+  }
+  public getCorrectName(src: string) {
+    console.log(src.split('/'));
   }
 }
